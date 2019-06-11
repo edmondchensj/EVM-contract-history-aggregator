5840dd1f79ebb0899cd8bca31e5bda218eb519e6
Edmond Chen <edmondchen@dhcp-10-105-219-183.wireless.northwestern.private>
2019-06-11 14:00:06 -0500

    modified visualizer

--- Graph/visualization.py
+++ Graph/visualization.py
@@ -1,75 +1,73 @@
 from graphviz import Digraph
 from pprint import pprint
-from GraphAggregator import GraphAggregator
+from Graph.GraphAggregator import GraphAggregator
+import json
+import os
 
 def test():
 
 
-	dot = Digraph(comment='The Round Table')
+    dot = Digraph(comment='The Round Table')
 
-	dot.node('1','1')
-	dot.node('2','2')
-	dot.node('11','11')
+    dot.node('1','1')
+    dot.node('2','2')
+    dot.node('11','11')
 
-	#dot.edges(['a1a2','a2a11'])
-	dot.edge('1', '2', constraint='true')
-	dot.edge('2', '11', constraint='true')
-
-
-	dot.view()
+    #dot.edges(['a1a2','a2a11'])
+    dot.edge('1', '2', constraint='true')
+    dot.edge('2', '11', constraint='true')
 
 
-def getGraph(output):
-	dot = Digraph(comment='Graph Aggregator')
-	nodes = []
-	
-	for key in output.keys():
-		if key not in nodes:
-			nodes.append(str(key))
-		for sub_key in output[key].keys():
-			if sub_key not in nodes:
-				nodes.append(str(sub_key))
-			
-	# for key in  output.keys():
-	# 	for sub_key in output[key].keys():
-	# 		if output[key][sub_key]['constraint']:
-
-	# 			dot.node(str(sub_key), str(sub_key), xlabel=str(output[key][sub_key]['constraint']))
-	# 			nodes.remove(str(sub_key))
-
-	for i in nodes:
-		dot.node(i, i)
-	for key in output.keys():
-		for sub_key in output[key].keys():
-			if output[key][sub_key]['constraint']:
-				dot.edge(str(key),str(sub_key), constraint='true', label=str(output[key][sub_key]['constraint']))
-			else:
-				dot.edge(str(key),str(sub_key), constraint='true')
+    dot.view()
 
 
-	dot.render('test-output/graph-aggregator.gv', view=True)  
-	
+def visualizeGraph(graph, output_fn):
+    dot = Digraph(comment='Graph Aggregator')
+    nodes = []
+    
+    for key in graph.keys():
+        if key not in nodes:
+            nodes.append(str(key))
+        for sub_key in graph[key].keys():
+            if sub_key not in nodes:
+                nodes.append(str(sub_key))
+            
+    # for key in  graph.keys():
+    #   for sub_key in graph[key].keys():
+    #       if graph[key][sub_key]['constraint']:
 
+    #           dot.node(str(sub_key), str(sub_key), xlabel=str(graph[key][sub_key]['constraint']))
+    #           nodes.remove(str(sub_key))
+
+    for i in nodes:
+        dot.node(i, i)
+    for key in graph.keys():
+        for sub_key in graph[key].keys():
+            if graph[key][sub_key]['constraint']:
+                dot.edge(str(key),str(sub_key), constraint='true', label=str(graph[key][sub_key]['constraint']))
+            else:
+                dot.edge(str(key),str(sub_key), constraint='true')
+
+
+    dot.render(output_fn, view=True)  
 
 def main():
-	test_inputs = [[1,2,4,5,7,8],
-				[1,3,4,6,7,9],
-				[1,3,4,5,7,8],
-				[1,3,4,10,7,9],
-				[1,3,11,4,10]]
-				
-	G = GraphAggregator()
-	G.make_graph(test_inputs, view_progress=True)
-	graph = G.get_graph()
-	print("Final graph: ")
-	pprint(graph)
+    folder = 'data/abnormal_traces/contract_folders'
+    
+    subfolders = [item for item in os.listdir(folder)
+                if os.path.isdir(os.path.join(folder, item))]
 
-	getGraph(graph)
+    for subf in subfolders:
+        subf_path = os.path.join(folder, subf)
+        graph_file = os.path.join(subf_path, 'graph.json')
 
+        with open(graph_file, 'r') as f:
+            graph = json.load(f)
+
+        visualizeGraph(graph, subf_path+'/graph')
 
 if __name__ == '__main__':
-	# This is the `main` call in python
-	main()
+    main()
 
 
 
